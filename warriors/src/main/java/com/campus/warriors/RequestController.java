5efe69d1ec739aea3498509fcb1c0d576121d032
xkr225 <yn.michel@gmail.com>
2019-11-15 16:36:18 +0100

    another commit

--- warriors/src/main/java/com/campus/warriors/RequestController.java
+++ warriors/src/main/java/com/campus/warriors/RequestController.java
@@ -3,26 +3,38 @@ package com.campus.warriors;
 import com.campus.warriors.contracts.*;
 import com.campus.warriors.engine.Game;
 import com.campus.warriors.engine.Warriors;
-import com.campus.warriors.model.Warrior;
+import com.google.common.collect.Iterables;
 import io.vavr.control.Option;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
 
 @RestController
 public class RequestController {
 
     private final static WarriorsAPI app = new Warriors();
 
-    @RequestMapping("/heroes")
+    @GetMapping("/heroes")
     public Iterable<Hero> heroes() {
         return app.availableHeroes();
     }
 
-    @RequestMapping("/maps")
+    @GetMapping("/maps")
     public Iterable<Map> maps() {
         return app.availableMaps();
+    }
+
+    @PostMapping("/games")
+    public GameState create(@RequestBody java.util.Map<String, Object> payload) {
+        String playerName = (String) payload.get("playerName");
+        int heroIndex = (int) payload.get("hero");
+        int mapIndex = (int) payload.get("map");
+
+        Iterable<Hero> heroes = app.availableHeroes();
+        Iterable<Map> maps = app.availableMaps();
+        Hero hero = Iterables.get(heroes, heroIndex);
+        Map map = Iterables.get(maps, mapIndex);
+
+        return app.createGame(playerName, hero, map);
     }
 
     @GetMapping("/games")
@@ -30,29 +42,10 @@ public class RequestController {
         return app.availableGames();
     }
 
-    @PostMapping("/games")
-    public GameState createGame(@RequestBody java.util.Map<String, Object> payload) {
-        System.out.println(payload);
-        System.out.println("GAMES");
-        Iterable<Hero> heroList = app.availableHeroes();
-        List<Hero> heroesList = new ArrayList<>();
-        heroList.forEach(heroesList::add);
-        Integer heroIndex = (Integer) payload.get("hero");
-        Hero hero = heroesList.get(heroIndex);
-        Iterable<Map> mapList = app.availableMaps();
-        List<Map> mapsList = new ArrayList<>();
-        mapList.forEach(mapsList::add);
-        Integer mapIndex = (Integer) payload.get("map");
-        Map map = mapsList.get(mapIndex);
-        return app.createGame((String) payload.get("playerName"), hero, map);
-    }
-
     @PostMapping("/games/{id}/turns")
-    Option<GameState> gameStateId(@PathVariable String id) {
-        System.out.println("TURN");
+    public Option<GameState> turns(@PathVariable String id) {
         GameId gameId = GameId.parse(id);
         return app.nextTurn(gameId);
     }
-
 
 }
